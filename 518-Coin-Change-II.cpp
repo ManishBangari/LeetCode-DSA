a4c1c894f6039ad46f9ae57b84c021b55eb3d388
Manish Singh <96987073+ManishBangari@users.noreply.github.com>
2023-08-11 23:39:45 +0530

    solved 518

---
+++ 518-Coin-Change-II.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        
+        int n=coins.size();
+        vector<int> count(amount+1);
+
+        count[0] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = coins[i]; j <= amount; j++) {
+                count[j] += count[j - coins[i]];
+            }
+        }
+
+        for(int i : count)  cout<<i<<' ';
+
+        return count[amount];
+    }
+};
